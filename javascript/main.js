860c22ffe2635f20d8c072ecdb5720056e772c29
juho-creator <72856990+juho-creator@users.noreply.github.com>
2023-07-27 17:16:23 +0900

    Update main.js
    메인 페이지 스크립트 추가 (Added script for the main page)

--- javascript/main.js
+++ javascript/main.js
@@ -1,45 +1,37 @@
-var isEnglish = true
+/* 메인 페이지 (Main Page) */
 
+// 언어 설정 변수 (Language setting variable)
+var isEnglish = true;
 
-
-// Get the "login" element by its ID
-const signupElement  = () =>
-{
-    window.location.href = "signup.html";    
+// 회원가입 페이지로 이동 (Redirect to the signup page)
+const signupElement = () => {
+    window.location.href = "signup.html";
 }
 
-    
-const ChangeLanguage = () =>
-{    
-    if(isEnglish === true)
-    {
-        document.getElementById("title").innerText = "AI 심리 상담사"
-        document.getElementById("title").style = "font-size:30px;"
+// 언어 변경 함수 (Function to change language)
+const ChangeLanguage = () => {
+    if (isEnglish === true) {
+        // 한국어로 설정 (Set to Korean)
+        document.getElementById("title").innerText = "AI 심리 상담사";
+        document.getElementById("title").style = "font-size: 30px;";
 
-        document.getElementById("name").innerText = "에밀리입니다."
-        document.getElementById("login").innerText = "로그인"
-        document.getElementById("signup").innerText = "회원가입"
+        document.getElementById("name").innerText = "에밀리입니다.";
+        document.getElementById("login").innerText = "로그인";
+        document.getElementById("signup").innerText = "회원가입";
         isEnglish = false;
-    }
+    } else if (isEnglish === false) {
+        // 영어로 설정 (Set to English)
+        document.getElementById("title").innerText = "Introducing your AI counselor";
+        document.getElementById("title").style = "font-size: 16px;";
 
-    else if (isEnglish === false)
-    {
-        document.getElementById("title").innerText = "Introducing your AI counselor"
-        document.getElementById("title").style = "font-size:16px;"
- 
-        document.getElementById("name").innerText = "Emily 😀"
-        document.getElementById("login").innerText = "login"
-        document.getElementById("signup").innerText = "signup"
+        document.getElementById("name").innerText = "Emily 😀";
+        document.getElementById("login").innerText = "login";
+        document.getElementById("signup").innerText = "signup";
         isEnglish = true;
     }
-
 }
 
-
-
-
-const loginElement = () =>
-{
+// 로그인 페이지로 이동 (Redirect to the login page)
+const loginElement = () => {
     window.location.href = "login.html";
-
 }
